8e45098a40d77358dd33c90ba88dabf60b635d10
Anjali Mathur <akmathur1321@gmail.com>
2025-02-22 17:15:15 -0600

    making the cards

--- frontend/src/article_card.jsx
+++ frontend/src/article_card.jsx
@@ -1,3 +1,4 @@
+import "./article_card.css";
 import { useState } from 'react';
 import { Container, Row, Col, Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,21 +8,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
  * @param {*} data includes whatever is given by then
  */
 function articleCard(data){
+    //const published = data.props.published
     console.log(data)
     return  <>
         <div className="card">
             <Row>
-                <p>{data.props.title}</p>
+                <p className="card_title">{data.props.title}</p>
             </Row>
             <Row>
-                <p>{data.props.description}</p>
+                <p className="card_description">{data.props.description}</p>
             </Row>
-            <Row>
+            <Row className="card_bottom_content">
                 <Col>
-                    <p>{data.props.source.name}</p>
+                    <p className="source_name">{data.props.source.name}</p>
                 </Col>
                 <Col>
-                    <p>{data.props.author}</p>
+                    <p className="author">{data.props.author}</p>
+                    <p className="author">{data.props.published}</p>
                 </Col>
             </Row>
         </div>
